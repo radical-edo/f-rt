36e9bd87a936807e46fdcfa9baece6e97d059de8
radical-edo <radical.3do@gmail.com>
2017-05-14 12:05:58 +0200

    Adding react inject tap plugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import PetsPage from './PetsPage';
 
