8ddacc5feee7d9f276d6d43d9209870d70da6855
radical-edo <radical.3do@gmail.com>
2017-05-16 23:19:08 +0200

    Styling the top nav buttons

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import 'font-awesome/css/font-awesome.min.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { NavLink, HashRouter as Router, Route } from 'react-router-dom';
 
 import ArticlesPage from './Articles/ArticlesPage';
 import PetsPage from './PetsPage';
 injectTapEventPlugin();
+import './index.css';
 
 const App = (props) =>
   <MuiThemeProvider>
     <Router>
       <div>
-        <nav>
-          <Link to="/articles">Articles</Link>
-          <Link to="/pets">Pets</Link>
+        <nav className="nav">
+          <NavLink className="nav__link" activeClassName="nav__link--active" to="/articles">Articles</NavLink>
+          <NavLink className="nav__link" activeClassName="nav__link--active" to="/pets">Pets</NavLink>
         </nav>
         <Route path="/articles" component={ArticlesPage} />
         <Route path="/pets" component={PetsPage} />
