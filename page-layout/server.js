e7dff289ac5f927216adba5a6c1c038641eaf831
radical-edo <radical.3do@gmail.com>
2017-05-18 08:45:27 +0200

    Styling top nav

--- page-layout/server.js
+++ page-layout/server.js
@@ -3,10 +3,15 @@ const path = require('path');
 
 const express = require('express');
 
-const PORT = 3001;
 const app = express();
+const PORT = 3001;
+const PUBLIC_PATH = path.join(__dirname, 'public');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(PUBLIC_PATH));
+
+app.get('/', function (req, res) {
+  res.sendFile(PUBLIC_PATH + '/guidelines.html');
+});
 
 app.listen(PORT, function () {
   console.log('Server listenting on', PORT);
